42c97d06a2c4ba453815c8dedee5da82acc5883e
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-07-13 12:36:43 -0300

    Adding trip controller

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -4,9 +4,12 @@ trips_routes_bp = Blueprint('trip_routes', __name__)
 
 from src.controllers.trip_creator import TripCretor
 from src.controllers.trip_finder import TripFinder
+from src.controllers.trip_confirmer import TripConfirmer
+from src.controllers.link_creator import LinkCreator
 
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.repositories.links_repository import LinksRepository
 
 from src.models.settings.db_connection_handler import db_connection_handler
 
@@ -30,4 +33,25 @@ def find_trip(trip_id):
   response = controller.find_trip_details(trip_id)
 
   return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<trip_id>/confirm', methods=['GET'])
+def confirm_trip(trip_id):
+  conn = db_connection_handler.get_connection()
+  trips_repository = TripsRepository(conn)
+  controller = TripConfirmer(trips_repository)
+
+  response = controller.confirm(trip_id)
+
+  return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<trip_id>/confirm', methods=['POST'])
+def create_trip_link(trip_id):
+  conn = db_connection_handler.get_connection()
+  links_repository = LinksRepository(conn)
+  controller = LinkCreator(links_repository)
+
+  response = controller.create(request.json, trip_id)
+
+  return jsonify(response['body']), response['status_code']
+
 
