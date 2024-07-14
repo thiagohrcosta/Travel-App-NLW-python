e9a602091e769a96bb45042726dccfc6c0241cd1
Thiagohrcosta <thiagohrcosta86@gmail.com>
2024-07-14 14:10:13 -0300

    Adding invites

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -5,12 +5,17 @@ trips_routes_bp = Blueprint('trip_routes', __name__)
 from src.controllers.trip_creator import TripCretor
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
+
 from src.controllers.link_creator import LinkCreator
 from src.controllers.link_finder import LinkFinder
+from src.controllers.participant_creator import ParticipantCreator
+from src.controllers.activity_creator import ActivityCreator
 
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
 from src.models.repositories.links_repository import LinksRepository
+from src.models.repositories.participants_repository import ParticipantsRepository
+from src.models.repositories.activities_repository import ActivitiesRepository
 
 from src.models.settings.db_connection_handler import db_connection_handler
 
@@ -64,3 +69,23 @@ def find_trip_link(trip_id):
   response = controller.find(trip_id)
 
   return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<trip_id>/invites", methods=["POST"])
+def invite_to_trip(trip_id):
+  conn = db_connection_handler.get_connection()
+  participants_repository = ParticipantRepository(conn)
+  emails_repository = EmailsToInviteRepository(conn)
+  controller = ParticipantCreator(participants_repository, emails_repository)
+
+  response = controller.create(request.json, trip_id)
+
+  return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<trip_id>/activities", methods=["POST"])
+def create_activity(trip_id):
+  conn = db_connection_handler.get_connection()
+  activities_repository = ActivitiesRepository(conn)
+  controller = ActivityCreator(activities_repository)
+
+  response = controller.create(request.json, trip_id)
+
